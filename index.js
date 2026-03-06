5d1feb2a0364cbe0a2d804c466cb2d8e73c9a1c6
Marissa Sanchez <A01385956@tec.mx>
2026-03-05 22:05:43 -0600

    Servidor, organicé Public, act de JS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static(__dirname));
+// Esto le dice a Express que busque todo lo visual dentro de "public"
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8080, () => {
     console.log("Servidor corriendo en http://localhost:8080");
 });
+
